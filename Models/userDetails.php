46f7da8792b6512f9395079753a047e781995a5e
Jeylin123 <jon2xlindo@gmail.com>
2017-12-03 15:56:37 +0800

    FUNCTIONS
    Added: 1.) customer crud
                 create, update, read
                 2.) Admin crud
                  create admin, create customer, read admin, deactivate user, activate user and delete user

---
+++ Models/userDetails.php
@@ -0,0 +1,261 @@
+<?php require_once("Database.php");
+    
+    class user_details{
+        private $userId;
+        private $username;
+        private $password;
+        private $userType;
+        private $userStatus;
+        private $gender;
+        private $email;
+        private $mobileNumber;
+        private $dateAdded;
+        private $dateUpdated;
+        private $addedBy;
+        private $updatedBy;
+        private $image;
+        private $address;
+
+        const DB_TABLE = "user_table";
+        const DB_TABLE_PK = "userId";
+    }
+
+    /***************** CONSTRUCTOR ****************/
+
+    public function __construct(){
+        
+    }
+
+    /***************** FUNCTIONS ****************/
+
+    public function createUser(){
+        setUsername($_POST['username']);
+        setPassword($_POST['password']);
+        setGender($_POST['gender']);
+        setEmail($_POST['email']);
+        setMobileNumber($_POST['mobileNumber']);
+        setAddress($_POST['address']);
+        if(isset($_SESSION)){
+            setAddedBy($_SESSION['userId'])
+        }else{
+            setAddedBy("NULL");
+        }
+        //setImage($_POST['image']);  i dont know if i should put an image sa creation.
+        setAddress($_POST['address']);
+        $create = "INSERT INTO user_details
+        ( 
+            userID, 
+            userName,
+            `password`, 
+            userType, 
+            userStatus,
+            gender,
+            email, 
+            mobileNumber,
+            addedBy,
+            `address`
+        )
+        VALUES
+        ('""','".getUserId()."','".getUsername()."','".getPassword()."','".getUserType()."','".getUserStatus()."','
+          ".getGender()."','".getEmail()."','".getMobileNumber()."','".getAddedBy()."','".getAddress()."')";
+        
+        $result = mysqli_query($mysqli, $create);
+        $select = "SELECT * 
+                   FROM  user_details 
+                   WHERE userName='".getUsername()."'";
+        $qry = mysqli_query($mysqli, $select);
+        $row = mysqli_fetch_array($qry);
+        setUserId($row[0]);
+        return getUserId();
+    }
+    
+    public function createAdmin(){
+        if(isset($_SESSION) && getUserType($_SESSION['userType']) == 'admin'){
+            setUserStatus('active');
+            setUserType('admin');
+            createUser();
+        }else{
+            echo 'only admins can create an admin';
+        }
+    }
+
+    public function activateUser(){
+        if(isset($_SESSION) && getUserType($_SESSION['userType']) == 'admin'){
+            setUserId($_POST['userId']);
+            $update = "UPDATE user_details
+                       SET userStatus = 'active' 
+                       WHERE userId = '".getUserId()."'
+                      ";
+            $result = mysqli_query($mysqli, $update);
+        }else{
+            echo 'only admins can activate a user';
+        }            
+    }
+
+    public function deactiveUser(){
+        if(isset($_SESSION) && getUserType($_SESSION['userType']) == 'admin'){
+            setUserId($_POST['userId']);
+            $update = "UPDATE user_details
+                       SET userStatus = 'inactive' 
+                       WHERE userId = '".getUserId()."'
+                      ";
+            $result = mysqli_query($mysqli, $update);
+        }else{
+            echo 'only admins can deactivate a user';
+        }
+    }
+
+    public function deleteUser(){
+        if(isset($_SESSION) && getUserType($_SESSION['userType']) == 'admin'){
+            $delete= "DELETE
+                      FROM user_table
+                      WHERE userID = '".$userID."' && userStatus = 'Not_Activated' 
+                     ";	
+            $result = mysqli_query($mysqli, $delete);
+            if(mysqli_num_rows($result) == 1){
+                alert("Delete: Succesful");
+            }else{
+                alert("Delete: Fail")
+            }
+        }else{
+            echo 'only admins can deactivate a user';
+        }
+        
+        
+        return result;
+    }
+
+    public function readAdmin(){
+        $query ="SELECT
+                        userId,
+                        username,
+                        userType,
+                        `status`,
+                        gender,
+                        email,
+                        mobileNumber,
+                        `image`,
+                        `address`
+                 FROM  user_details
+                 WHERE userId = '".getUserId()."'
+                ");
+        $row = mysqli_query($mysqli, $query);
+        $result = mysqli_fetch_array($row);
+
+        return result;
+    }
+
+    /***************** SETTERS AND GETTERS ****************/
+
+    public function getUserId(){
+        return $this->user_id;
+    }
+
+    public function setUserId($userId){
+        $this->userId = $userId;
+    }
+
+    public function getUsername(){
+        return $this->username; 
+    }
+
+    public function setUsername($username){
+        $this->username = $username;
+    }
+
+    public function getPassword(){
+        return $this->password;
+    }
+
+    public function setPassword($password){
+        $this->password = $password;
+    }
+    
+    public function getUserType(){
+        return $this->userType;
+    }
+
+    public function setUserType($userType){
+        $this->userType = $userType;
+    }
+
+    public function getUserStatus(){
+        return $this->userStatus;
+    }
+
+    public function setUserStatus($userStatus){
+        $this->userStatus = $userStatus;
+    }
+
+    public function getGender(){
+        return $this->gender;
+    }
+
+    public function setGender($gender){
+        $this->gender = $gender;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    public function getMobileNumber(){
+        return $this->mobileNumber;
+    }
+
+    public function setMobileNumber($mobileNumber){
+        $this->mobileNumber = $mobileNumber;
+    }
+
+    public function getDateAdded(){
+        return $this->dateAdded;
+    }
+
+    public function setDateAdded($dateAdded){
+        $this->dateAdded = $dateAdded;
+    }
+
+    public function getDateUpdated(){
+        return $this->dateUpdated;
+    }
+
+    public function setDateUpdated($dateUpdated){
+        $this->dateUpdated = $dateUpdated;
+    }
+
+    public function getAddedBy(){
+        return $this->addedBy;
+    }
+
+    public function setAddedBy($addedBy){
+        $this->addedBy = $addedBy;
+    }
+
+    public function getUpdatedBy(){
+        return $this->updatedBy;
+    }
+
+    public function setUpdatedBy($updatedBy){
+        $this->updatedBy = $updatedBy;
+    }
+
+    public function getImage(){
+        return $this->image;
+    }
+
+    public function setImage($image){
+        $this->image = $image;
+    }
+
+    public function getAddress(){
+        return $this->address;
+    }
+
+    public function setAddress($address){
+        $this->address = $address;
+    }
+?>
---
+++ kwarto_db.sql
@@ -0,0 +1,401 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 01, 2017 at 10:01 AM
+-- Server version: 10.1.24-MariaDB
+-- PHP Version: 7.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kwarto_remaked`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `categoryId` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `dateAdded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `addedBy` int(11) NOT NULL,
+  `updatedBy` int(11) NOT NULL,
+  `parent` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customerId` int(11) NOT NULL,
+  `firstName` varchar(256) NOT NULL,
+  `middleName` varchar(256) DEFAULT NULL,
+  `lastName` varchar(256) NOT NULL,
+  `birthdate` date NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_order`
+--
+
+CREATE TABLE `customer_order` (
+  `orderId` int(11) NOT NULL,
+  `orderNumber` int(11) NOT NULL,
+  `orderDate` date NOT NULL,
+  `status` enum('placed','cancelled','fulfilled') NOT NULL,
+  `fulfilled` tinyint(1) NOT NULL DEFAULT '0',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `paid` tinyint(1) NOT NULL DEFAULT '0',
+  `paymentDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateAdded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `addedBy` int(11) NOT NULL,
+  `updatedBy` int(11) NOT NULL,
+  `customerId` int(11) NOT NULL,
+  `paymentId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `furniture`
+--
+
+CREATE TABLE `furniture` (
+  `furnitureId` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `description` varchar(512) NOT NULL,
+  `length` float NOT NULL,
+  `width` float NOT NULL,
+  `height` float NOT NULL,
+  `rating` float NOT NULL,
+  `price` float NOT NULL,
+  `unit` char(5) NOT NULL,
+  `dateAdded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `addedBy` int(11) NOT NULL,
+  `updatedBy` int(11) NOT NULL,
+  `modelName` varchar(256) DEFAULT NULL,
+  `discount` float NOT NULL,
+  `categoryId` int(11) NOT NULL,
+  `sellerId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `furniture_image`
+--
+
+CREATE TABLE `furniture_image` (
+  `furnitureImageId` int(11) NOT NULL,
+  `image` varchar(256) NOT NULL,
+  `dateAdded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `addedBy` int(11) NOT NULL,
+  `updatedBy` int(11) NOT NULL,
+  `furnitureId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_item`
+--
+
+CREATE TABLE `order_item` (
+  `orderItemId` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `discount` float NOT NULL,
+  `totalPrice` float NOT NULL,
+  `dateAdded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `addedBy` int(11) NOT NULL,
+  `updatedBy` int(11) NOT NULL,
+  `orderId` int(11) NOT NULL,
+  `furnitureId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `paymentId` int(11) NOT NULL,
+  `type` enum('cash','credit','free') NOT NULL,
+  `dateAdded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `addedBy` int(11) NOT NULL,
+  `updatedBy` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+CREATE TABLE `review` (
+  `reviewId` int(11) NOT NULL,
+  `rating` float NOT NULL,
+  `likes` int(11) NOT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  `body` varchar(512) DEFAULT NULL,
+  `dateAdded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `addedBy` int(11) NOT NULL,
+  `updatedBy` int(11) NOT NULL,
+  `furnitureId` int(11) NOT NULL,
+  `customerId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seller`
+--
+
+CREATE TABLE `seller` (
+  `sellerId` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `description` varchar(512) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_details`
+--
+
+CREATE TABLE `user_details` (
+  `userId` int(11) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `userType` enum('admin','customer','seller') NOT NULL DEFAULT 'customer',
+  `userStatus` enum('inactive','active','banned') NOT NULL DEFAULT 'inactive',
+  `gender` enum('Male','Female','Other') NOT NULL DEFAULT 'Male',
+  `email` varchar(256) NOT NULL,
+  `mobileNumber` varchar(16) DEFAULT NULL,
+  `dateAdded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `addedBy` int(11) DEFAULT NULL,
+  `updatedBy` int(11) NOT NULL,
+  `image` varchar(256) DEFAULT NULL,
+  `address` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`categoryId`),
+  ADD KEY `category_FK` (`parent`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customerId`),
+  ADD KEY `customer_FK` (`userId`);
+
+--
+-- Indexes for table `customer_order`
+--
+ALTER TABLE `customer_order`
+  ADD PRIMARY KEY (`orderId`),
+  ADD KEY `customer_order_FK1` (`customerId`),
+  ADD KEY `customer_order_FK2` (`paymentId`);
+
+--
+-- Indexes for table `furniture`
+--
+ALTER TABLE `furniture`
+  ADD PRIMARY KEY (`furnitureId`),
+  ADD KEY `furniture_FK1` (`categoryId`),
+  ADD KEY `furniture_FK2` (`sellerId`);
+
+--
+-- Indexes for table `furniture_image`
+--
+ALTER TABLE `furniture_image`
+  ADD PRIMARY KEY (`furnitureImageId`),
+  ADD KEY `furniture_image_FK` (`furnitureId`);
+
+--
+-- Indexes for table `order_item`
+--
+ALTER TABLE `order_item`
+  ADD PRIMARY KEY (`orderItemId`),
+  ADD KEY `order_item_FK1` (`orderId`),
+  ADD KEY `order_item_FK2` (`furnitureId`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`paymentId`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`reviewId`),
+  ADD KEY `review_FK1` (`furnitureId`),
+  ADD KEY `review_FK2` (`customerId`);
+
+--
+-- Indexes for table `seller`
+--
+ALTER TABLE `seller`
+  ADD PRIMARY KEY (`sellerId`),
+  ADD KEY `seller_FK` (`userId`);
+
+--
+-- Indexes for table `user_details`
+--
+ALTER TABLE `user_details`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `categoryId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customerId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `customer_order`
+--
+ALTER TABLE `customer_order`
+  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `furniture`
+--
+ALTER TABLE `furniture`
+  MODIFY `furnitureId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `furniture_image`
+--
+ALTER TABLE `furniture_image`
+  MODIFY `furnitureImageId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `order_item`
+--
+ALTER TABLE `order_item`
+  MODIFY `orderItemId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `paymentId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `reviewId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `seller`
+--
+ALTER TABLE `seller`
+  MODIFY `sellerId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_details`
+--
+ALTER TABLE `user_details`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `category`
+--
+ALTER TABLE `category`
+  ADD CONSTRAINT `category_FK` FOREIGN KEY (`parent`) REFERENCES `category` (`categoryId`);
+
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `customer_FK` FOREIGN KEY (`userId`) REFERENCES `user_details` (`userId`);
+
+--
+-- Constraints for table `customer_order`
+--
+ALTER TABLE `customer_order`
+  ADD CONSTRAINT `customer_order_FK1` FOREIGN KEY (`customerId`) REFERENCES `customer` (`customerId`),
+  ADD CONSTRAINT `customer_order_FK2` FOREIGN KEY (`paymentId`) REFERENCES `payment` (`paymentId`);
+
+--
+-- Constraints for table `furniture`
+--
+ALTER TABLE `furniture`
+  ADD CONSTRAINT `furniture_FK1` FOREIGN KEY (`categoryId`) REFERENCES `category` (`categoryId`),
+  ADD CONSTRAINT `furniture_FK2` FOREIGN KEY (`sellerId`) REFERENCES `seller` (`sellerId`);
+
+--
+-- Constraints for table `furniture_image`
+--
+ALTER TABLE `furniture_image`
+  ADD CONSTRAINT `furniture_image_FK` FOREIGN KEY (`furnitureId`) REFERENCES `furniture` (`furnitureId`);
+
+--
+-- Constraints for table `order_item`
+--
+ALTER TABLE `order_item`
+  ADD CONSTRAINT `order_item_FK1` FOREIGN KEY (`orderId`) REFERENCES `customer_order` (`orderId`),
+  ADD CONSTRAINT `order_item_FK2` FOREIGN KEY (`furnitureId`) REFERENCES `furniture` (`furnitureId`);
+
+--
+-- Constraints for table `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `review_FK1` FOREIGN KEY (`furnitureId`) REFERENCES `furniture` (`furnitureId`),
+  ADD CONSTRAINT `review_FK2` FOREIGN KEY (`customerId`) REFERENCES `customer` (`customerId`);
+
+--
+-- Constraints for table `seller`
+--
+ALTER TABLE `seller`
+  ADD CONSTRAINT `seller_FK` FOREIGN KEY (`userId`) REFERENCES `user_details` (`userId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
