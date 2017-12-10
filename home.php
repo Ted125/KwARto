2455847971b213630b88f016d7b794a757fbf7f1
Leonille Christie Lavador <leonillechristie@gmail.com>
2017-12-11 00:17:20 +0800

    Added new homepage but not yet linked to other pages

---
+++ home.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <?php include'Access/Header.php'; ?>
+
+  </head>
+  <style>
+
+    <?php include'Access/Style.php'; ?>
+
+  </style>
+  <body>
+<!-- header -->
+    <?php include'Access/Navbar.php'; ?>
+<div>	
+<!-- events -->
+		<img src="images/header.jpg" style="padding: 0;">
+		<div class="events">
+			<h3>What is KwARto?</h3>
+			<div class="w3agile_event_grids">
+				<div class="col-md-6 w3agile_event_grid">
+					<div class="col-md-3 w3agile_event_grid_left">
+						<i class="fa fa-bullhorn" aria-hidden="true"></i>
+					</div>
+					<div class="col-md-9 w3agile_event_grid_right">
+						<h4>eCommerce Platform</h4>
+						<p>KwARto makes buying and selling furniture much more convenient!</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="col-md-6 w3agile_event_grid">
+					<div class="col-md-3 w3agile_event_grid_left">
+						<i class="fa fa-bullseye" aria-hidden="true"></i>
+					</div>
+					<div class="col-md-9 w3agile_event_grid_right">
+						<h4>Scope of the platform</h4>
+						<p>Services are pertained only to customers in the Philippines!</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="w3agile_event_grids">
+				<div class="col-md-6 w3agile_event_grid">
+					<div class="col-md-3 w3agile_event_grid_left">
+						<i class="fa fa-credit-card" aria-hidden="true"></i>
+					</div>
+					<div class="col-md-9 w3agile_event_grid_right">
+						<h4>How does payment work?</h4>
+						<p>We have several modes of payment available namely: Banking, Cash on Delivery and Online Payment</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="col-md-6 w3agile_event_grid">
+					<div class="col-md-3 w3agile_event_grid_left">
+						<i class="fa fa-eye" aria-hidden="true"></i>
+					</div>
+					<div class="col-md-9 w3agile_event_grid_right">
+						<h4>What Makes KWARTO Special?</h4>
+						<p>KwARto's mobile application integrates Augmented Reality. Make sure to check that out!</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="events-bottom">
+				<div class="col-md-6 events_bottom_left">
+					<div class="col-md-4 events_bottom_left1">
+						<div class="events_bottom_left1_grid">
+							<h4>12</h4>
+							<p>December 2017</p>
+						</div>
+					</div>
+					<div class="col-md-8 events_bottom_left2">	
+						<img src="http://www.alakazam.com.tr/eng/wp-content/uploads/2015/08/sleep-839358_1920-1-demo-400x266.jpg" alt=" " class="img-responsive" />
+						<h4>Announcement</h4>
+						<ul>
+							<li><i class="fa fa-clock-o" aria-hidden="true"></i>3:00 PM</li>
+							<li><i class="fa fa-user" aria-hidden="true"></i><a href="#">Admin</a></li>
+						</ul>
+						<p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil 
+							impedit quo minus id quod maxime placeat facere possimus, omnis voluptas 
+							assumenda est.</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="col-md-6 events_bottom_left">
+					<div class="col-md-4 events_bottom_left1">
+						<div class="events_bottom_left1_grid">
+							<h4>12</h4>
+							<p>December 2017</p>
+						</div>
+					</div>
+					<div class="col-md-8 events_bottom_left2">	
+						<img src="http://www.alakazam.com.tr/eng/wp-content/uploads/2015/08/sleep-839358_1920-1-demo-400x266.jpg" alt=" " class="img-responsive" />
+						<h4>Important Announcement</h4>
+						<ul>s
+							<li><i class="fa fa-clock-o" aria-hidden="true"></i>3:30 PM</li>
+							<li><i class="fa fa-user" aria-hidden="true"></i><a href="#">Admin</a></li>
+						</ul>
+						<p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil 
+							impedit quo minus id quod maxime placeat facere possimus, omnis voluptas 
+							assumenda est.</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+<!-- //events -->
+		</div>
+		<div class="clearfix"></div>
+	</div>
+<!-- //banner -->
+<!-- newsletter -->
+	<div class="newsletter">
+		<div class="container">
+			<div class="w3agile_newsletter_left">
+				<h3>sign up for our newsletter</h3>
+			</div>
+			<div class="w3agile_newsletter_right">
+				<form action="#" method="post">
+					<input type="email" name="Email" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}" required="">
+					<input type="submit" value="subscribe now">
+				</form>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+<!-- //newsletter -->
+<!-- footer -->
+	<div class="footer">
+		<div class="container">
+			<div class="col-md-3 w3_footer_grid">
+				<h3>information</h3>
+				<ul class="w3_footer_grid_list">
+					<li><a href="events.html">Events</a></li>
+					<li><a href="about.html">About Us</a></li>
+					<li><a href="products.html">Best Deals</a></li>
+					<li><a href="services.html">Services</a></li>
+					<li><a href="short-codes.html">Short Codes</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 w3_footer_grid">
+				<h3>policy info</h3>
+				<ul class="w3_footer_grid_list">
+					<li><a href="faqs.html">FAQ</a></li>
+					<li><a href="privacy.html">privacy policy</a></li>
+					<li><a href="privacy.html">terms of use</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 w3_footer_grid">
+				<h3>what in stores</h3>
+				<ul class="w3_footer_grid_list">
+					<li><a href="pet.html">Pet Food</a></li>
+					<li><a href="frozen.html">Frozen Snacks</a></li>
+					<li><a href="kitchen.html">Kitchen</a></li>
+					<li><a href="products.html">Branded Foods</a></li>
+					<li><a href="household.html">Households</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 w3_footer_grid">
+				<h3>twitter posts</h3>
+				<ul class="w3_footer_grid_list1">
+					<li><label class="fa fa-twitter" aria-hidden="true"></label><i>01 day ago</i><span>Non numquam <a href="#">http://sd.ds/13jklf#</a>
+						eius modi tempora incidunt ut labore et
+						<a href="#">http://sd.ds/1389kjklf#</a>quo nulla.</span></li>
+					<li><label class="fa fa-twitter" aria-hidden="true"></label><i>02 day ago</i><span>Con numquam <a href="#">http://fd.uf/56hfg#</a>
+						eius modi tempora incidunt ut labore et
+						<a href="#">http://fd.uf/56hfg#</a>quo nulla.</span></li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+			<div class="agile_footer_grids">
+				<div class="col-md-3 w3_footer_grid agile_footer_grids_w3_footer">
+					<div class="w3_footer_grid_bottom">
+						<h4>100% secure payments</h4>
+						<img src="images/card.png" alt=" " class="img-responsive" />
+					</div>
+				</div>
+				<div class="col-md-3 w3_footer_grid agile_footer_grids_w3_footer">
+					<div class="w3_footer_grid_bottom">
+						<h5>connect with us</h5>
+						<ul class="agileits_social_icons">
+							<li><a href="#" class="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="#" class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							<li><a href="#" class="google"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+							<li><a href="#" class="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+							<li><a href="#" class="dribbble"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="wthree_footer_copy">
+				<p>© 2016 Grocery Store. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+			</div>
+		</div>
+	</div>
+<!-- //footer -->
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+<!-- //here ends scrolling icon -->
+<script src="js/minicart.js"></script>
+<script>
+		paypal.minicart.render();
+
+		paypal.minicart.cart.on('checkout', function (evt) {
+			var items = this.items(),
+				len = items.length,
+				total = 0,
+				i;
+
+			// Count the number of each item in the cart
+			for (i = 0; i < len; i++) {
+				total += items[i].get('quantity');
+			}
+
+			if (total < 3) {
+				alert('The minimum order quantity is 3. Please add more to your shopping cart before checking out');
+				evt.preventDefault();
+			}
+		});
+
+	</script>
+</body>
+</html>
