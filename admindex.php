7d7d172bf027c90864d22e183ee79efba19bd67d
Leonille Christie Lavador <leonillechristie@gmail.com>
2017-12-23 19:51:07 +0800

    Minor changes to home and reports for admin

--- admindex.php
+++ admindex.php
@@ -124,12 +124,6 @@
                 <!-- Work Amount  -->
                 <div class="col-lg-6">
                   <div class="work-amount card">
-                    <div class="card-close">
-                      <div class="dropdown">
-                        <button type="button" id="closeCard1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-ellipsis-v"></i></button>
-                        <div aria-labelledby="closeCard1" class="dropdown-menu dropdown-menu-right has-shadow"><a href="#" class="dropdown-item remove"> <i class="fa fa-times"></i>Close</a><a href="#" class="dropdown-item edit"> <i class="fa fa-gear"></i>Edit</a></div>
-                      </div>
-                    </div>
                     <div class="card-body">
                       <h3>Average Demographic</h3><small>Average number of hours users spend on the website according to age group</small>
                       <div class="chart text-center">
--- adminrep.php
+++ adminrep.php
@@ -157,12 +157,6 @@
                 <!-- Line Charts-->
                 <div class="col-lg-8">
                   <div class="line-chart-example card">
-                    <div class="card-close">
-                      <div class="dropdown">
-                        <button type="button" id="closeCard1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-ellipsis-v"></i></button>
-                        <div aria-labelledby="closeCard1" class="dropdown-menu dropdown-menu-right has-shadow"><a href="#" class="dropdown-item remove"> <i class="fa fa-times"></i>Close</a><a href="#" class="dropdown-item edit"> <i class="fa fa-gear"></i>Edit</a></div>
-                      </div>
-                    </div>
                     <div class="card-header d-flex align-items-center">
                       <h3 class="h4">Registered Users</h3>
                     </div>
