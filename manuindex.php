609b11d580961a67f46d86b1620693f4f3893cf2
Leonille Christie Lavador <leonillechristie@gmail.com>
2017-12-26 11:47:33 +0800

    Completed Manufacturer dashboard

---
+++ manuindex.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>kwARto 2018</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <!-- Fontastic Custom icon font-->
+    <link rel="stylesheet" href="css/fontastic.css">
+    <!-- Google fonts - Poppins -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,700">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+ 
+    <!-- Web Icon -->
+    <link rel="shortcut icon" href="images/icon.png">
+   
+  </head>
+  <body>
+    <div class="page">
+      <!-- Main Navbar-->
+      <header class="header">
+        <nav class="navbar">
+          <!-- Search Box-->
+          <div class="search-box">
+            <button class="dismiss"><i class="icon-close"></i></button>
+            <form id="searchForm" action="#" role="search">
+              <input type="search" placeholder="What are you looking for..." class="form-control">
+            </form>
+          </div>
+          <div class="container-fluid">
+            <div class="navbar-holder d-flex align-items-center justify-content-between">
+              <!-- Navbar Header-->
+              <div class="navbar-header">
+                <!-- Navbar Brand --><a href="admindex.php" class="navbar-brand">
+                  <div class="brand-text brand-big"><span>kw </span><strong>AR</strong>to</div>
+                  <div class="brand-text brand-small"><strong>AR</strong></div></a>
+                <!-- Toggle Button--><a id="toggle-btn" href="#" class="menu-btn active"><span></span><span></span><span></span></a>
+              </div>
+              <!-- Navbar Menu -->
+              <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
+                <!-- Search-->
+                <li class="nav-item d-flex align-items-center"><a id="search" href="#"><i class="icon-search"></i></a></li>
+               
+                <!-- Logout    -->
+                <li class="nav-item"><a href="loginnew.php" class="nav-link logout">Logout<i class="fa fa-sign-out"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <div class="page-content d-flex align-items-stretch"> 
+        <!-- Side Navbar -->
+        <nav class="side-navbar">
+          <!-- Sidebar Header-->
+          <div class="sidebar-header d-flex align-items-center">
+            <div class="avatar"><img src="https://www.shareicon.net/data/2016/07/05/791221_man_512x512.png" alt="..." class="img-fluid rounded-circle"></div>
+            <div class="title">
+              <h1 class="h4">Company Name</h1>
+              <p>Manufacturer</p>
+            </div>
+          </div>
+          <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
+          <ul class="list-unstyled">
+                    <li class="active"><a href="manuindex.php"> <i class="icon-list"></i>Reports</a></li>
+                    <li><a href="prodsmanu.php"> <i class="icon-padnote"></i>Products Management</a></li>
+                    <li><a href="upload.php"> <i class="icon-grid"></i>Upload Product</a></li>
+ 
+          </ul><span class="heading">Extras</span>
+          <ul class="list-unstyled">
+            <li> <a href="manuprofile.php"> <i class="icon-user"></i>Profile </a></li>
+            <li><a href="loginnew.php"> <i class="icon-interface-windows"></i>Logout</a></li>
+          </ul>
+        </nav>
+        <div class="content-inner">
+          <!-- Page Header-->
+          <header class="page-header">
+            <div class="container-fluid">
+              <h2 class="no-margin-bottom">Reports</h2>
+            </div>
+          </header>
+          <!-- Breadcrumb-->
+          <div class="breadcrumb-holder container-fluid">
+            <ul class="breadcrumb">
+              <li class="breadcrumb-item"><a href="admindex.php">Home</a></li>
+              <li class="breadcrumb-item active">Reports </li>
+            </ul>
+          </div>
+          <!-- Charts Section-->
+          <section class="charts" style="background-color: #faf6f6">
+
+            <section class="dashboard-counts no-padding-bottom" style="padding-top: 0px;">
+            <div class="container-fluid">
+              <div class="row bg-white has-shadow">
+                <!-- Item -->
+                <div class="col-xl-3 col-sm-6">
+                  <div class="item d-flex align-items-center">
+                    <div class="icon bg-violet"><i class="fa-money"></i></div>
+                    <div class="title"><span>Monthly<br>Sold</span>
+                      <div class="progress">
+                        <div role="progressbar" style="width: 25%; height: 4px;" aria-valuenow="{#val.value}" aria-valuemin="0" aria-valuemax="100" class="progress-bar bg-violet"></div>
+                      </div>
+                    </div>
+                    <div class="number"><strong>29</strong></div>
+                  </div>
+                </div>
+                <!-- Item -->
+                <div class="col-xl-3 col-sm-6">
+                  <div class="item d-flex align-items-center">
+                    <div class="icon bg-red"><i class="fa fa-comment-o"></i></div>
+                    <div class="title"><span>New<br>Comments</span>
+                      <div class="progress">
+                        <div role="progressbar" style="width: 70%; height: 4px;" aria-valuenow="{#val.value}" aria-valuemin="0" aria-valuemax="100" class="progress-bar bg-red"></div>
+                      </div>
+                    </div>
+                    <div class="number"><strong>70</strong></div>
+                  </div>
+                </div>
+                <!-- Item -->
+                <div class="col-xl-3 col-sm-6">
+                  <div class="item d-flex align-items-center">
+                    <div class="icon bg-green"><i class="fa fa-star"></i></div>
+                    <div class="title"><span>New<br>Ratings</span>
+                      <div class="progress">
+                        <div role="progressbar" style="width: 40%; height: 4px;" aria-valuenow="{#val.value}" aria-valuemin="0" aria-valuemax="100" class="progress-bar bg-green"></div>
+                      </div>
+                    </div>
+                    <div class="number"><strong>40</strong></div>
+                  </div>
+                </div>
+                <!-- Item -->
+                <div class="col-xl-3 col-sm-6">
+                  <div class="item d-flex align-items-center">
+                    <div class="icon bg-orange"><i class="fa fa-warning"></i></div>
+                    <div class="title"><span>New<br>Questions</span>
+                      <div class="progress">
+                        <div role="progressbar" style="width: 50%; height: 4px;" aria-valuenow="{#val.value}" aria-valuemin="0" aria-valuemax="100" class="progress-bar bg-orange"></div>
+                      </div>
+                    </div>
+                    <div class="number"><strong>12</strong></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <br>
+            <div class="container-fluid">
+              <div class="row">
+                <!-- Line Charts-->
+                <div class="col-lg-8">
+                  <div class="line-chart-example card">
+                    <div class="card-header d-flex align-items-center">
+                      <h3 class="h4">Total Sales</h3>
+                    </div>
+                    <div class="card-body">
+                      <canvas id="lineChartExample"></canvas>
+                      <!-- reused these charts. just make new ones in charts-custom.js .. templated na to-->
+                    </div>
+                  </div>
+                </div>
+
+                <div class="statistics col-lg-4 col-12">
+                  <div class="statistic d-flex align-items-center bg-white has-shadow">
+                    <div class="icon bg-red"><i class="fa fa-bed"></i></div>
+                    <div class="text"><strong>234</strong><br><small>Added Products</small></div>
+                  </div>
+                  <div class="statistic d-flex align-items-center bg-white has-shadow">
+                    <div class="icon bg-green"><i class="fa fa-rub"></i></div>
+                    <div class="text"><strong>152</strong><br><small>Completed Transactions</small></div>
+                  </div>
+                  <div class="statistic d-flex align-items-center bg-white has-shadow">
+                    <div class="icon bg-orange"><i class="fa fa-mail-reply"></i></div>
+                    <div class="text"><strong>10</strong><br><small>Product Returns</small></div>
+                  </div>
+                </div>
+                
+                <div class="col-lg-12">
+                  <div class="line-chart-example card">
+                    <div class="card-header d-flex align-items-center">
+                      <h3 class="h4">Top Selling Products</h3>
+                    </div>
+                    <div class="card-body">
+                      <canvas id="lineChartExample2"></canvas>
+                    </div>
+                  </div>
+                </div>
+          </section>
+          <!-- Page Footer-->
+          <footer class="main-footer">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-sm-6 col-lg-12 text-right">
+                  <p>kwARto &copy; 2018-2019</p>
+                </div>
+                
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </div>
+    <!-- Javascript files-->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
+    <script src="js/charts-custom.js"></script>
+    <!-- Main File-->
+    <script src="js/front.js"></script>
+  </body>
+</html>
