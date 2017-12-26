52adc424aba975218cb06665ab25679b8db3fd94
onalag <galanoroald@gmail.com>
2017-12-26 12:38:13 +0800

    Revert "Fixed Niggy-zation of the Website"
    This reverts commit 58bf6d1f71d9d319de6f7e410135608e5437f00c.

--- home.php
+++ home.php
@@ -1,44 +1,193 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
 
     <?php include'Access/Header.php'; ?>
 
-</head>
-<style>
+  </head>
+  <style>
 
-<?php include'Access/Style.php'; ?>
+    <?php include'Access/Style.php'; ?>
 
-</style>
-<body>
-
+  </style>
+  <body>
+<!-- header -->
     <?php include'Access/Navbar.php'; ?>
-    <!-- <?php include'Access/RegisterForm.php'; ?> -->
-    <div class="backdrop" style="padding-left: 200px; padding-right: 200px;">
-        <br>
-        <br>
-        <div class="w3ls_w3l_banner_nav_right_grid">
-            <h3>This Week's Deals!</h3>
-            <div class="w3ls_w3l_banner_nav_right_grid1">
-                <h6>Decor</h6>
-                    <?php include'Access/CardProd.php'; ?>
-                    <?php include'Access/CardProd.php'; ?>
-                <div class="clearfix"> </div>
-            </div>
-            <div class="clearfix"> </div>
-        </div>
-    </div>
-</div>
-<div class="clearfix"></div>
-</div>
+<div>
+<!-- events -->
+               <img src="images/header.jpg" style="padding: 0;">
+               <div class="events">
+                       <h3>What is KwARto?</h3>
+                       <div class="w3agile_event_grids">
+                               <div class="col-md-6 w3agile_event_grid">
+                                       <div class="col-md-3 w3agile_event_grid_left">
+                                               <i class="fa fa-bullhorn" aria-hidden="true"></i>
+                                       </div>
+                                       <div class="col-md-9 w3agile_event_grid_right">
+                                               <h4>eCommerce Platform</h4>
+                                               <p>KwARto makes buying and selling furniture much more convenient!</p>
+                                       </div>
+                                       <div class="clearfix"> </div>
+                               </div>
+                               <div class="col-md-6 w3agile_event_grid">
+                                       <div class="col-md-3 w3agile_event_grid_left">
+                                               <i class="fa fa-bullseye" aria-hidden="true"></i>
+                                       </div>
+                                       <div class="col-md-9 w3agile_event_grid_right">
+                                               <h4>Scope of the platform</h4>
+                                               <p>Services are pertained only to customers in the Philippines!</p>
+                                       </div>
+                                       <div class="clearfix"> </div>
+                               </div>
+                               <div class="clearfix"> </div>
+                       </div>
+                       <div class="w3agile_event_grids">
+                               <div class="col-md-6 w3agile_event_grid">
+                                       <div class="col-md-3 w3agile_event_grid_left">
+                                               <i class="fa fa-credit-card" aria-hidden="true"></i>
+                                       </div>
+                                       <div class="col-md-9 w3agile_event_grid_right">
+                                               <h4>How does payment work?</h4>
+                                               <p>We have several modes of payment available namely: Banking, Cash on Delivery and Online Payment</p>
+                                       </div>
+                                       <div class="clearfix"> </div>
+                               </div>
+                               <div class="col-md-6 w3agile_event_grid">
+                                       <div class="col-md-3 w3agile_event_grid_left">
+                                               <i class="fa fa-eye" aria-hidden="true"></i>
+                                       </div>
+                                       <div class="col-md-9 w3agile_event_grid_right">
+                                               <h4>What Makes KWARTO Special?</h4>
+                                               <p>KwARto's mobile application integrates Augmented Reality. Make sure to check that out!</p>
+                                       </div>
+                                       <div class="clearfix"> </div>
+                               </div>
+                               <div class="clearfix"> </div>
+                       </div>
+                       <div class="events-bottom">
+                               <div class="col-md-6 events_bottom_left">
+                                       <div class="col-md-8 events_bottom_left2">
+                                               <img src="images/ar.png" alt=" " class="img-responsive" style="min-height: 150%; min-width: 150%;" />
+                                       </div>
+                                       <div class="clearfix"> </div>
+                               </div>
+                               <div class="col-md-6 events_bottom_left">
+                                       <div class=" text-center col-md-8 events_bottom_left2">
+                                               <h3>Get the mobile app!</h3>
+                                               <p>Download the kwARto mobile application in the Google Play Store today to obtain the full experience! <br> <br> Mobile Application Exclusive Features: <br>Augmented Reality Preview of furniture products!<br>Handheld shopping experience!<br>Blah blah blah!<br>Augmented Reality Preview of furniture products!</p>
+                                       </div>
+                                       <div class="clearfix"> </div>
+                               </div>
+                               <div class="clearfix"> </div>
+                       </div>
+                       <div class="events-bottom">
+                               <div class="col-md-6 events_bottom_left">
+                                       <div class="col-md-4 events_bottom_left1">
+                                               <div class="events_bottom_left1_grid">
+                                                       <h4>12</h4>
+                                                       <p>December 2017</p>
+                                               </div>
+                                       </div>
+                                       <div class="col-md-8 events_bottom_left2">
+                                               <img src="http://www.alakazam.com.tr/eng/wp-content/uploads/2015/08/sleep-839358_1920-1-demo-400x266.jpg" alt=" " class="img-responsive" />
+                                               <h4>Announcement</h4>
+                                               <ul>
+                                                       <li><i class="fa fa-clock-o" aria-hidden="true"></i>3:00 PM</li>
+                                                       <li><i class="fa fa-user" aria-hidden="true"></i><a href="#">Admin</a></li>
+                                               </ul>
+                                               <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
+                                                       impedit quo minus id quod maxime placeat facere possimus, omnis voluptas
+                                                       assumenda est.</p>
+                                       </div>
+                                       <div class="clearfix"> </div>
+                               </div>
+                               <div class="col-md-6 events_bottom_left">
+                                       <div class="col-md-4 events_bottom_left1">
+                                               <div class="events_bottom_left1_grid">
+                                                       <h4>12</h4>
+                                                       <p>December 2017</p>
+                                               </div>
+                                       </div>
+                                       <div class="col-md-8 events_bottom_left2">
+                                               <img src="http://www.alakazam.com.tr/eng/wp-content/uploads/2015/08/sleep-839358_1920-1-demo-400x266.jpg" alt=" " class="img-responsive" />
+                                               <h4>Important Announcement</h4>
+                                               <ul>s
+                                                       <li><i class="fa fa-clock-o" aria-hidden="true"></i>3:30 PM</li>
+                                                       <li><i class="fa fa-user" aria-hidden="true"></i><a href="#">Admin</a></li>
+                                               </ul>
+                                               <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
+                                                       impedit quo minus id quod maxime placeat facere possimus, omnis voluptas
+                                                       assumenda est.</p>
+                                       </div>
+                                       <div class="clearfix"> </div>
+                               </div>
+                               <div class="clearfix"> </div>
+                       </div>
+               </div>
+               </div>
+<!-- //events -->
+               </div>
+               <div class="clearfix"></div>
+       </div>
 <!-- //banner -->
 <!-- newsletter -->
+       <div class="newsletter">
+               <div class="container">
+                       <div class="w3agile_newsletter_left">
+                               <h3>sign up for our newsletter</h3>
+                       </div>
+                       <div class="w3agile_newsletter_right">
+                               <form action="#" method="post">
+                                       <input type="email" name="Email" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}" required="">
+                                       <input type="submit" value="subscribe now">
+                               </form>
+                       </div>
+                       <div class="clearfix"> </div>
+               </div>
+       </div>
+<!-- //newsletter -->
+    <!-- footer -->
+    <div class="footer">
+        <div class="container">
+            <div class="col-md-3 w3_footer_grid">
+                <h3>information</h3>
+                <ul class="w3_footer_grid_list">
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="mail.html">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 w3_footer_grid">
+                <h3>policy info</h3>
+                <ul class="w3_footer_grid_list">
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">privacy policy</a></li>
+                    <li><a href="#">terms of use</a></li>
+                </ul>
+            </div>
+            <div class="clearfix"> </div>
+            <div class="agile_footer_grids">
+                <div class="col-md-3 w3_footer_grid agile_footer_grids_w3_footer">
+                    <div class="w3_footer_grid_bottom">
+                        <h5>connect with us</h5>
+                        <ul class="agileits_social_icons">
+                            <li><a href="#" class="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="#" class="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                            <li><a href="#" class="google"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                            <li><a href="#" class="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                            <li><a href="#" class="dribbble"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+    </div>
+    <!-- //footer -->
 
-<!-- //footer -->
-<?php include'Access/Footer.php'; ?>
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
-<script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <script>
     $(document).ready(function() {
         $(".dropdown").hover(
             function() {
@@ -49,26 +198,26 @@
                 $('.dropdown-menu', this).stop(true, true).slideUp("fast");
                 $(this).toggleClass('open');
             }
-            );
+        );
     });
-</script>
-<!-- here stars scrolling icon -->
-<script type="text/javascript">
+    </script>
+    <!-- here stars scrolling icon -->
+    <script type="text/javascript">
     $(document).ready(function() {
         $().UItoTop({ easingType: 'easeOutQuart' });
 
     });
-</script>
-<!-- //here ends scrolling icon -->
-<script src="js/minicart.js"></script>
-<script>
+    </script>
+    <!-- //here ends scrolling icon -->
+    <script src="js/minicart.js"></script>
+    <script>
     paypal.minicart.render();
 
     paypal.minicart.cart.on('checkout', function(evt) {
         var items = this.items(),
-        len = items.length,
-        total = 0,
-        i;
+            len = items.length,
+            total = 0,
+            i;
 
         // Count the number of each item in the cart
         for (i = 0; i < len; i++) {
@@ -80,11 +229,11 @@
             evt.preventDefault();
         }
     });
-</script>
-<!-- /.container -->  
-<?php include'Access/ScriptLinks.php'; ?>
+    </script>
+    <!-- /.container -->
+    <?php include'Access/ScriptLinks.php'; ?>
 
 
-</body>
+  </body>
 
 </html>
