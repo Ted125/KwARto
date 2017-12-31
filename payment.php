f5e169ef146334326e59fe89eb798bb7fb7ffc39
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-01-01 03:45:42 +0800

    Added Payment/checkout page

---
+++ payment.php
@@ -0,0 +1,331 @@
+n<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>KwARto | Web Application</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Colo Shop Template">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" href="plugins/themify-icons/themify-icons.css">
+<link rel="stylesheet" type="text/css" href="plugins/jquery-ui-1.12.1.custom/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="styles/single_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/single_responsive.css">
+<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+<link rel="icon" href="images/icon.png">
+</head>
+
+<body>
+
+<div class="super_container">
+
+	<header class="header trans_300">
+		<div class="top_nav">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<div class="top_nav_left">Sign Up Now and avail free shipping off your first purchase!</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+
+		<div class="main_nav_container">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-right">
+						<div class="logo_container">
+							<a href="#"><img style="max-width: 150px;" src="images/newlogo.png"></a>
+						</div>
+						<nav class="navbar">
+							<ul class="navbar_menu">
+								<li><a href="index.php">home</a></li>
+								<li><a href="categories.php">categories</a></li>
+								<li><a href="about.php">about us</a></li>
+								<li><a href="contact.php">contact</a></li>
+							</ul>
+							<ul class="navbar_user">
+
+								<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+								<li><a href="profile.php"><i class="fa fa-user" aria-hidden="true"></i></a>
+
+								</li>
+								<li class="checkout">
+									<a href="cart.php">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										<span id="checkout_items" class="checkout_items">2</span>
+									</a>
+								</li>
+							</ul>
+							<div class="hamburger_container">
+								<i class="fa fa-bars" aria-hidden="true"></i>
+							</div>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	
+	<div class="container single_product_container">
+		<div class="row">
+			<div class="col">
+
+				<div class="breadcrumbs d-flex flex-row align-items-center" style="margin-bottom: 10px;">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li class="active"><a href="payment.php"><i class="fa fa-angle-right" aria-hidden="true"></i>Payment</a></li>
+					</ul>
+				</div>
+
+			</div>
+		</div>
+
+	<div class="blogs">
+		<div class="container">
+			<h2><i class="fa fa-money" aria-hidden="true"></i> Checkout</h2>
+			<div class="row">
+				<div class="col text-center">
+					<div class="section_title">
+						<h3>Select Your Payment Method</h3>
+					</div>
+				</div>
+			</div>
+			<div class="row blogs_container">
+				<div class="col-lg-4 blog_item_col">
+					<div class="blog_item">
+						<div class="blog_background" style="background-image:url(https://rpsstudio.co/assets/images/icons/cash.svg)"></div>
+						<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
+							<h4 class="blog_title">Cash On Delivery</h4>
+							<span class="blog_meta">Pay to courier upon receiving</span>
+							<a class="" data-toggle="modal" data-target="#myModal" style="text-decoration: none; color: #d42d2d;" href="#">Select</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 blog_item_col">
+					<div class="blog_item">
+						<div class="blog_background" style="background-image:url(http://www.pvhc.net/img138/vhfubfultznbnkaozkjh.png)"></div>
+						<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
+							<h4 class="blog_title">Credit or Debit Card</h4>
+							<span class="blog_meta">Visa | Mastercard</span>
+							<a class="" style="text-decoration: none; color: #d42d2d;" href="#">Select</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 blog_item_col">
+					<div class="blog_item">
+						<div class="blog_background" style="background-image:url(https://www.sketchappsources.com/resources/source-image/PayPalCard.png)"></div>
+						<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
+							<h4 class="blog_title">Paypal</h4>
+							<span class="blog_meta">You will be redirected</span>
+							<a class="" style="text-decoration: none; color: #d42d2d;" href="https://www.paypal.com/PH">Select</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+	</div>
+	
+
+	<div class="benefit">
+		<div class="container">
+			<div class="row benefit_row">
+				<div class="col-lg-3 benefit_col">
+					<div class="benefit_item d-flex flex-row align-items-center">
+						<div class="benefit_icon"><i class="fa fa-truck" aria-hidden="true"></i></div>
+						<div class="benefit_content">
+							<h6>free shipping</h6>
+							<p>Within the Philippines</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 benefit_col">
+					<div class="benefit_item d-flex flex-row align-items-center">
+						<div class="benefit_icon"><i class="fa fa-money" aria-hidden="true"></i></div>
+						<div class="benefit_content">
+							<h6>cash on delivery</h6>
+							<p>Pay upon receiving your item</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 benefit_col">
+					<div class="benefit_item d-flex flex-row align-items-center">
+						<div class="benefit_icon"><i class="fa fa-undo" aria-hidden="true"></i></div>
+						<div class="benefit_content">
+							<h6>14 days return</h6>
+							<p>See return policy for details</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 benefit_col">
+					<div class="benefit_item d-flex flex-row align-items-center">
+						<div class="benefit_icon"><i class="fa fa-clock-o" aria-hidden="true"></i></div>
+						<div class="benefit_content">
+							<h6>Open Always</h6>
+							<p>24/7</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="newsletter">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="newsletter_text d-flex flex-column justify-content-center align-items-lg-start align-items-md-center text-center">
+						<h4>Newsletter</h4>
+						<p>Subscribe to our newsletter and get 20% off your first purchase</p>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<form action="post">
+						<div class="newsletter_form d-flex flex-md-row flex-column flex-xs-column align-items-center justify-content-lg-end justify-content-center">
+							<input id="newsletter_email" type="email" placeholder="Your email" required="required" data-error="Valid email is required.">
+							<button id="newsletter_submit" type="submit" class="newsletter_submit_btn trans_300" value="Submit">subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer -->
+
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
+						<ul class="footer_nav">
+							<li><a href="terms.php">Terms of Use</a></li>
+							<li><a href="privacy.php">Privacy Policy</a></li>
+							<li><a href="contact.php">Contact us</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="footer_nav_container">
+						<div class="cr">©2018 All Rights Reserverd. <a href="#">KwARto</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	
+	<!-- MODAL CONTENTS -->
+	<!-- CASH ON DELIVERY -->
+	<div id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" class="modal fade text-left">
+	    <div role="document" class="modal-dialog modal-bigger" style="min-width: 800px!important;">
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <h4 id="exampleModalLabel" class="modal-title">CASH ON DELIVERY</h4>
+	          <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button>
+	        </div>
+	        <div class="modal-body">
+	        	<h5>Ship to:</h5>
+	          <form>
+	            <div class="form-group">
+	              <label>Full Name</label>
+	              <input type="text" placeholder="John Smith" class="form-control">
+	            </div>
+	            <div class="form-group">       
+	              <label>Complete Address (House Number, Building and Street Name)</label>
+	              <textarea class="form-control" rows="3" id="address" placeholder="12B, ABC Blg, 123 Internet St."></textarea>
+	            </div>
+	            <div class="form-group">       
+	              <label>Province</label>
+	              <select name="account" class="form-control">
+	                <option>Province</option>
+	                <option>Option 2</option>
+	                <option>Option 3</option>
+	                <option>Option 4</option>
+	              </select>
+	            </div>
+	            <div class="form-group">       
+	              <label>City/Municipality</label>
+	              <select name="account" class="form-control">
+	                <option>City/Municipality</option>
+	                <option>Option 2</option>
+	                <option>Option 3</option>
+	                <option>Option 4</option>
+	              </select>
+	            </div>
+	            <div class="form-group">       
+	              <label>Barangay</label>
+	              <select name="account" class="form-control">
+	                <option>Barangay</option>
+	                <option>Option 2</option>
+	                <option>Option 3</option>
+	                <option>Option 4</option>
+	              </select>
+	            </div>
+	            <div class="form-group">       
+	              <label>Phone Number</label>
+	              <input type="text" placeholder="0912 345 6789" class="form-control">
+	            </div>
+
+	            <h6>Delivery Options:</h6>
+	            <div class="row">
+		            <div class="blog_item" style="width: 250px;">
+						<div class="blog_background" style="min-height: auto; background-image:url(https://d30y9cdsu7xlg0.cloudfront.net/png/92358-200.png)"></div>
+						<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
+							<span>Standard Delivery</span><br>
+							<h6>Sat 6, Mon | 22 Jan 2018</h6>
+							<span class="blog_meta">P99.00</span>
+						</div>
+					</div>
+
+					<div class="blog_item" style="width: 250px; margin-left: 20px;">
+						<div class="blog_background" style="min-height: auto; background-image:url(https://d30y9cdsu7xlg0.cloudfront.net/png/100245-200.png)"></div>
+						<div class="blog_content d-flex flex-column align-items-center justify-content-center text-center">
+							<span>Rush Delivery</span><br>
+							<h6>Sat 2, Mon | 5 Jan 2018</h6>
+							<span class="blog_meta">P249.00</span>
+						</div>
+					</div>
+				</div>
+
+	            <div class="form-group">
+	              <p>Please review all information before proceeding.</p>       
+	              <input type="submit" value="PROCEED TO PAY" class="btn red_button" style="color: white;">
+	            </div>
+	          </form>
+	        </div>
+	    </div>
+  </div>
+  <!-- END OF CASH ON DELIVERY -->
+
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/jquery-ui-1.12.1.custom/jquery-ui.js"></script>
+<script src="js/single_custom.js"></script>
+</body>
+
+</html>
