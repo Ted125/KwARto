4f691e51574f84374efad537ae789144f206a4a6
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-01-08 22:55:59 +0800

    added new page news.php for single page of news in index page

---
+++ news.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>KwARto | Web Application</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Colo Shop Template">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+<link rel="icon" href="images/icon.png">
+</head>
+
+<body>
+
+<div class="super_container">
+
+	<header class="header trans_300">
+
+		<div class="top_nav">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<div class="top_nav_left">Sign Up Now and avail free shipping off your first purchase!</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+
+		<div class="main_nav_container">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-right">
+						<div class="logo_container">
+							<a href="#"><img style="max-width: 150px;" src="images/newlogo.png"></a>
+						</div>
+						<nav class="navbar">
+							<ul class="navbar_menu">
+								<li><a href="index.php">home</a></li>
+								<li><a href="categories.php">categories</a></li>
+								<li><a href="about.php">about us</a></li>
+								<li><a href="contact.php">contact</a></li>
+							</ul>
+							<ul class="navbar_user">
+								<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+								<li><a href="profile.php"><i class="fa fa-user" aria-hidden="true"></i></a>
+								</li>
+								<li class="checkout">
+									<a href="cart.php">
+										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										<span id="checkout_items" class="checkout_items">2</span>
+									</a>
+								</li>
+							</ul>
+							<div class="hamburger_container">
+								<i class="fa fa-bars" aria-hidden="true"></i>
+							</div>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div class="main_slider" style="background-image:url(http://cdn.abclocal.go.com/content/kfsn/images/cms/automation/images/1915021_1280x720.jpg)">
+		<div class="container fill_height">
+			<div class="row align-items-center fill_height">
+				<div class="col">
+					<div class="main_slider_content">
+						<h6 style="margin-bottom: 10px; margin-top: -100px; color: white;">Latest News</h6>
+						<!-- <h1>KwARto</h1> -->
+						<h1 style="color: white;">Year Ender Sale</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="best_sellers">
+		<div class="container">
+			<div class="row">
+				<div class="col text-center">
+					<div class="section_title new_arrivals_title">
+						<h2>Year Ender Promos</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<div class="product_slider_container">
+						<div class="owl-carousel owl-theme product_slider">
+
+							<div class="owl-item product_slider_item">
+								<div class="product-item homeoff">
+									<div class="product discount">
+										<div class="product_image">
+											<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXx4epDpWIT7a-4AYBLk5xDhaJ_ZpboHnMc5jFbcnSygD98Tnp" style="padding-top: 20px; max-height: 240px;">
+										</div>
+										<div class="favorite"></div>
+										<div class="product_info">
+											<h6 class="product_name"><a href="single.php">Floral Divider</a></h6>
+											<div class="product_price">P1,180.00</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="owl-item product_slider_item">
+								<div class="product-item homeacc">
+									<div class="product">
+										<div class="product_image">
+											<img src="https://i.pinimg.com/736x/42/7d/09/427d09befbe59e3d47f29eebd63e762b--bedroom-desk-bedroom-office.jpg" style="padding-top: 20px; max-height: 240px;">
+										</div>
+										<div class="favorite"></div>
+										<div class="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center"><span>-P1000</span></div>
+										<div class="product_info">
+											<h6 class="product_name"><a href="#single.php">Blue Bedroom Desk</a></h6>
+											<div class="product_price">P4,000<span>P5,000.00</span></div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="owl-item product_slider_item">
+								<div class="product-item homeacc">
+									<div class="product">
+										<div class="product_image">
+											<img src="https://www.patiowarehouse.co.za/wp-content/uploads/2017/10/YorkB-6Seater-400x400.jpg" style="padding-top: 20px; max-height: 240px;">
+										</div>
+										<div class="favorite favorite_left"></div>
+										<div class="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center"><span>-P500</span></div>
+										<div class="product_info">
+											<h6 class="product_name"><a href="#single.php">Dining Room Set (White)</a></h6>
+											<div class="product_price">P4,500<span>P5,000.00</span></div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="owl-item product_slider_item">
+								<div class="product-item dining">
+									<div class="product">
+										<div class="product_image">
+											<img src="http://www.ikea.com/PIAimages/0312397_PE429728_S5.JPG" style="padding-top: 20px; max-height: 240px;" alt="">
+										</div>
+										<div class="favorite"></div>
+										<div class="product_bubble product_bubble_left product_bubble_green d-flex flex-column align-items-center"><span>new</span></div>
+										<div class="product_info">
+											<h6 class="product_name"><a href="single.php">Leatherette Sofa</a></h6>
+											<div class="product_price">P3,610.00</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="owl-item product_slider_item">
+								<div class="product-item women men">
+									<div class="product">
+										<div class="product_image">
+											<img src="https://images.samsclubresources.com/is/image/samsclub/0004216738171_A?$img_size_380x380$" style="padding-top: 20px; max-height: 240px;" alt="">
+										</div>
+										<div class="favorite favorite_left"></div>
+										<div class="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center"><span>-10%</span></div>
+										<div class="product_info">
+											<h6 class="product_name"><a href="single.php">Swivel Chair (Black)</a></h6>
+											<div class="product_price">P540.00<span>P600.00</span></div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							
+							<div class="owl-item product_slider_item">
+								<div class="product-item homeoff">
+									<div class="product discount">
+										<div class="product_image">
+											<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXx4epDpWIT7a-4AYBLk5xDhaJ_ZpboHnMc5jFbcnSygD98Tnp" style="padding-top: 20px; max-height: 240px;">
+										</div>
+										<div class="favorite"></div>
+										<div class="product_info">
+											<h6 class="product_name"><a href="single.php">Floral Divider</a></h6>
+											<div class="product_price">P1,180.00</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="owl-item product_slider_item">
+								<div class="product-item homeacc">
+									<div class="product">
+										<div class="product_image">
+											<img src="https://i.pinimg.com/736x/42/7d/09/427d09befbe59e3d47f29eebd63e762b--bedroom-desk-bedroom-office.jpg" style="padding-top: 20px; max-height: 240px;">
+										</div>
+										<div class="favorite"></div>
+										<div class="product_info">
+											<h6 class="product_name"><a href="single.php">Blue Bedroom Desk</a></h6>
+											<div class="product_price">P1,500.00</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="owl-item product_slider_item">
+								<div class="product-item homeacc">
+									<div class="product">
+										<div class="product_image">
+											<img src="https://www.patiowarehouse.co.za/wp-content/uploads/2017/10/YorkB-6Seater-400x400.jpg" style="padding-top: 20px; max-height: 240px;">
+										</div>
+										<div class="favorite favorite_left"></div>
+										<div class="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center"><span>-P500</span></div>
+										<div class="product_info">
+											<h6 class="product_name"><a href="#single.php">Dining Room Set (White)</a></h6>
+											<div class="product_price">P4,500<span>P5,000.00</span></div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="owl-item product_slider_item">
+								<div class="product-item dining">
+									<div class="product">
+										<div class="product_image">
+											<img src="http://www.ikea.com/PIAimages/0312397_PE429728_S5.JPG" style="padding-top: 20px; max-height: 240px;" alt="">
+										</div>
+										<div class="favorite"></div>
+										<div class="product_bubble product_bubble_left product_bubble_green d-flex flex-column align-items-center"><span>new</span></div>
+										<div class="product_info">
+											<h6 class="product_name"><a href="single.php">Leatherette Sofa</a></h6>
+											<div class="product_price">P3,610.00</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="owl-item product_slider_item">
+								<div class="product-item women men">
+									<div class="product">
+										<div class="product_image">
+											<img src="https://images.samsclubresources.com/is/image/samsclub/0004216738171_A?$img_size_380x380$" style="padding-top: 20px; max-height: 240px;" alt="">
+										</div>
+										<div class="favorite favorite_left"></div>
+										<div class="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center"><span>-10%</span></div>
+										<div class="product_info">
+											<h6 class="product_name"><a href="single.php">Swivel Chair (Black)</a></h6>
+											<div class="product_price">P540.00<span>P600.00</span></div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="product_slider_nav_left product_slider_nav d-flex align-items-center justify-content-center flex-column">
+							<i class="fa fa-chevron-left" aria-hidden="true"></i>
+						</div>
+						<div class="product_slider_nav_right product_slider_nav d-flex align-items-center justify-content-center flex-column">
+							<i class="fa fa-chevron-right" aria-hidden="true"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<br><p>Lorem ipsum dolor sit amet, odio timeam docendi per at. An tacimates laboramus vituperata mei, cum no detraxit antiopam expetendis, ut nam partem audiam. Te facer error vituperatoribus per, omnis illum ea mei. Dolores facilisi ad sed, latine aliquip duo ne, nisl atomorum quo at. Ex sea quidam noster iudicabit, sea feugait aliquando te.Movet erroribus mei eu, cu tota solum partiendo vix. Ut autem laboramus usu, vis eligendi partiendo ex. Voluptua scaevola ad his. Cu mel purto dictas singulis. Ut pro feugait tibique dissentias.<br><br>Alienum deleniti an est, pri option ancillae te. Vocent vulputate accommodare at vel. Cum et vivendum quaestio. Posse noster veritus ne nec, lucilius antiopam honestatis ius ea, at sit epicurei expetenda voluptaria.<br><br>Adhuc vituperatoribus id eam, nam harum dissentias at. Eu partem volutpat assentior eum, autem fugit dictas ex cum, ea wisi tollit scripserit mea. Ius te graeco euripidis constituam, vel adhuc facete ei. Nam cibo feugait ea. Pri id epicurei reformidans, ei mei solum iriure laoreet. Id mei munere hendrerit contentiones, sit et alia modus, est iudico laoreet et.</p>
+	</div>
+
+
+	<div class="benefit">
+		<div class="container">
+			<div class="row benefit_row">
+				<div class="col-lg-3 benefit_col">
+					<div class="benefit_item d-flex flex-row align-items-center">
+						<div class="benefit_icon"><i class="fa fa-truck" aria-hidden="true"></i></div>
+						<div class="benefit_content">
+							<h6>free shipping</h6>
+							<p>Within the Philippines</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 benefit_col">
+					<div class="benefit_item d-flex flex-row align-items-center">
+						<div class="benefit_icon"><i class="fa fa-money" aria-hidden="true"></i></div>
+						<div class="benefit_content">
+							<h6>cash on delivery</h6>
+							<p>Pay upon receiving your item</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 benefit_col">
+					<div class="benefit_item d-flex flex-row align-items-center">
+						<div class="benefit_icon"><i class="fa fa-undo" aria-hidden="true"></i></div>
+						<div class="benefit_content">
+							<h6>14 days return</h6>
+							<p>See return policy for details</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 benefit_col">
+					<div class="benefit_item d-flex flex-row align-items-center">
+						<div class="benefit_icon"><i class="fa fa-clock-o" aria-hidden="true"></i></div>
+						<div class="benefit_content">
+							<h6>Open Always</h6>
+							<p>24/7</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="newsletter">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="newsletter_text d-flex flex-column justify-content-center align-items-lg-start align-items-md-center text-center">
+						<h4>Newsletter</h4>
+						<p>Subscribe to our newsletter and get 20% off your first purchase</p>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<form action="post">
+						<div class="newsletter_form d-flex flex-md-row flex-column flex-xs-column align-items-center justify-content-lg-end justify-content-center">
+							<input id="newsletter_email" type="email" placeholder="Your email" required="required" data-error="Valid email is required.">
+							<button id="newsletter_submit" type="submit" class="newsletter_submit_btn trans_300" value="Submit">subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
+						<ul class="footer_nav">
+							<li><a href="terms.php">Terms of Use</a></li>
+							<li><a href="privacy.php">Privacy Policy</a></li>
+							<li><a href="contact.php">Contact us</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="footer_nav_container">
+						<div class="cr">©2018 All Rights Reserverd. <a href="#">KwARto</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="js/custom.js"></script>
+</body>
+
+</html>
