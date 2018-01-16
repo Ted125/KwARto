7b7b0d68683cb6ec7cbbe2ea9b3323ada5b0b49f
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-01-16 17:18:08 +0800

    new single.php

--- single.php
+++ single.php
@@ -22,54 +22,8 @@ n<!DOCTYPE html>
 
 <div class="super_container">
 
-	<header class="header trans_300">
-		<div class="top_nav">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12 text-center">
-						<div class="top_nav_left">Sign Up Now and avail free shipping off your first purchase!</div>
-					</div>
-					
-				</div>
-			</div>
-		</div>
-
-		<div class="main_nav_container">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 text-right">
-						<div class="logo_container">
-							<a href="index.php"><img style="max-width: 150px;" src="images/newlogo.png"></a>
-						</div>
-						<nav class="navbar">
-							<ul class="navbar_menu">
-								<li><a href="index.php">home</a></li>
-								<li><a href="categories.php">categories</a></li>
-								<li><a href="about.php">about us</a></li>
-								<li><a href="contact.php">contact</a></li>
-							</ul>
-							<ul class="navbar_user">
-
-								<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
-								<li><a href="profile.php"><i class="fa fa-user" aria-hidden="true"></i></a>
-
-								</li>
-								<li class="checkout">
-									<a href="cart.php">
-										<i class="fa fa-shopping-cart" aria-hidden="true"></i>
-										<span id="checkout_items" class="checkout_items">2</span>
-									</a>
-								</li>
-							</ul>
-							<div class="hamburger_container">
-								<i class="fa fa-bars" aria-hidden="true"></i>
-							</div>
-						</nav>
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
+		<!-- NAVBAR HERE -->
+	<?php include('Access/Navbar.php');?>	
 
 	<div class="fs_menu_overlay"></div>
 
@@ -96,7 +50,7 @@ n<!DOCTYPE html>
 						<div class="col-lg-3 thumbnails_col order-lg-1 order-2">
 							<div class="single_product_thumbnails">
 								<ul>
-									<li ><img src="" alt="" data-image="./images/single/front2.jpg"></li>
+									<li ><img src="./images/single/front2.jpg" alt="" data-image="./images/single/front2.jpg"></li>
 									<li class="active"><img src="./images/single/front.jpg" alt="" data-image="./images/single/front.jpg"></li>
 									<li><img src="./images/single/back.jpg"></li>
 								</ul>
@@ -284,7 +238,7 @@ n<!DOCTYPE html>
 												<li><i class="fa fa-star" aria-hidden="true"></i></li>
 												<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
 											</ul>
-											<textarea id="review_message" class="input_review" name="message"  placeholder="Your Review" rows="4" required data-error="Please leave us a review."></textarea>
+											<textarea id="review_message" class="input_review" name="body"  placeholder="Your Review" rows="4" required data-error="Please leave us a review."></textarea>
 										</div>
 										<div class="text-left text-sm-right">
 											<button id="review_submit" type="submit" class="red_button review_submit_btn trans_300" value="Submit">submit</button>
