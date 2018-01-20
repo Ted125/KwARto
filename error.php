8b6ef0204d0a8d34f2402b01342bf41ee5966de8
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-01-20 15:32:50 +0800

    Added error page

---
+++ error.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>KwARto | Web Application</title>
+
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/font-awesome.css" rel="stylesheet" type="text/css" media="all" />
+	<link rel="icon" type="image/png" href="images/icon.png" />
+
+	<script src="js/jquery-1.11.1.min.js"></script>
+
+	<link href='//fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+	<!-- smooth-scrolling -->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event) {
+				event.preventDefault();
+				$('html,body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
+			});
+		});
+	</script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <link href="./css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./css/now-ui-kit.css?v=1.1.0" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300" rel="stylesheet">
+
+  </head>
+  <style>
+
+  </style>
+        <body class="page-header clear-filter" filter-color="orange" style="background-image: url(https://pf2cdn.haworth.com/sites/poltronafrau.com/files/styles/homepage_link/public/savina_renzofrau_0.jpg?itok=un9AJy2Q); margin: 0px; background-size: 100%; background-repeat: no-repeat;">
+    <div class="wrapper">
+            <div class="page-header-image" data-parallax="true" style="">
+            </div>
+            <div class="container">
+                <div class="content-center brand" style="margin-top:35%;">
+                    <img src="./images/largerw.png" alt="">
+                    <h1 style="font-family: 'Source Sans Pro', sans-serif!important; font-size: 3em;"class="h1-seo">ERROR 404</h1><br><br>
+                    <h3 style="font-family: 'Source Sans Pro', sans-serif!important; font-size: 2em;">Page not found.</h3>
+                </div>
+            </div>
+        </div>
+	<div>	
+  </body>
+
+</html>
