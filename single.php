650461d6154fb6f159055773f42ea5643013bd61
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-01-23 22:03:56 +0800

    ui fix for button

--- single.php
+++ single.php
@@ -473,7 +473,7 @@
 											<textarea id="review_message" class="input_review" name="body"  placeholder="Your Review" rows="4" required data-error="Please leave us a review."></textarea>
 										</div>
 										<div class="text-left text-sm-right">
-											<button id="review_submit" type="submit" class="red_button review_submit_btn trans_300" value="Submit">submit</button>
+											<button id="review_submit" type="submit" style="padding: 10px;" class="red_button review_submit_btn trans_300" value="Submit">submit</button>
 										</div>
 									</form>
 								</div>
@@ -563,7 +563,7 @@
 											<textarea id="review_message" class="input_review" name="question"  placeholder="Your Question" rows="4" required data-error="Please leave us a question."></textarea>
 										</div>
 										<div class="text-left text-sm-right">
-											<button id="review_submit" type="submit" class="red_button review_submit_btn trans_300" value="Submit">submit</button>
+											<button id="review_submit" type="submit" style="padding: 10px;" class="red_button review_submit_btn trans_300" value="Submit">submit</button>
 										</div>
 									</form>
 								</div>
