00108eced0e0cd7ea416696dea6bd15ae442a07c
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-01-30 17:17:57 +0800

    tab and mobile fix

--- home.php
+++ home.php
@@ -148,7 +148,7 @@
 	
 	<div class="tab-block">
 		<div class="text-center"><img style="margin-top: 20%;" src="./images/largerr.png" alt="">
-        <h1 style="font-family: 'Source Sans Pro', sans-serif!important; margin-top: 10px; color: black; font-size: 3em;"class="h1-seo">KW<span style="color: #d42d2d;">AR</span>TO</h1><br><br>
+        <h3 style="font-family: 'Source Sans Pro', sans-serif!important; margin-top: 10px; color: black; font-size: 3em;">KW<span style="color: #d42d2d;">AR</span>TO</h3><br><br>
         <h3 style="font-family: 'Source Sans Pro', sans-serif!important;color: black; font-size: 2em; margin-top: -20px;">Revolutionizing online furniture shopping.</h3>
 	    <p style="padding-bottom: 300px;font-family: 'Source Sans Pro', sans-serif!important; color: black;">Download the mobile app now!</p>
 
