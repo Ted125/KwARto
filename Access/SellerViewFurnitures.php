267f65ae52ac2ae0f1780d74f58550883f1def0e
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-01-30 10:33:23 +0800

    added hover titles for buttons

--- Access/SellerViewFurnitures.php
+++ Access/SellerViewFurnitures.php
@@ -21,9 +21,9 @@
   <td class="row">
     <!-- button for Toggle live --> 
     <form method="post" action="Controllers/SellerToggleLive.php">
-      <input type="hidden" value=<?php echo $row['live'];?> name="live" />
+      <input type="hidden" value=<?php echo $row['live'];?> name="live"/>
       <input type="hidden" value=<?php echo $row['furnitureId'];?> name="furnitureId"/>
-      <button type="submit" class="btn btn-primary <?php echo $ban_warn;?>" <?php echo $ban;?>></button>
+      <button type="submit" title="Live/Remove from live" class="btn btn-primary <?php echo $ban_warn;?>" <?php echo $ban;?>></button>
     </form>
 
     <!-- Modal-->
@@ -106,7 +106,7 @@
       </div>
     </div>
     <!-- Button for Restock Furnitures -->
-    <button type="button" data-toggle="modal" data-target="<?php echo "#restockModal".$count;?>" class="btn btn-primary fa fa-plus"></button>
+    <button type="button" title="Restock Product" data-toggle="modal" data-target="<?php echo "#restockModal".$count;?>" class="btn btn-primary fa fa-plus"></button>
     <div id=<?php echo "restockModal".$count;?> tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" class="modal fade text-left">
       <div role="document" class="modal-dialog">
         <div class="modal-content">
@@ -138,7 +138,7 @@
       </div>
     </div>
     <!-- Button for Discount -->
-    <button type="button" data-toggle="modal" data-target="<?php echo "#sale".$count;?>" class="btn btn-primary fa fa-gift"></button>
+    <button type="button" title="Add Promo/Discount" data-toggle="modal" data-target="<?php echo "#sale".$count;?>" class="btn btn-primary fa fa-gift"></button>
     <div id=<?php echo "sale".$count;?> tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" class="modal fade text-left">
       <div role="document" class="modal-dialog">
         <div class="modal-content">
