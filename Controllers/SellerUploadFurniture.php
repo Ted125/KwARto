13e8d165882160afe4d2d1d5f0abceeec02072e2
Christian Ted Ochoa <ochoatechmaster25@gmail.com>
2018-02-02 20:45:56 +0800

    fixed roalds seller upload redirect bug

--- Controllers/SellerUploadFurniture.php
+++ Controllers/SellerUploadFurniture.php
@@ -50,7 +50,7 @@ if ($verify != null || $verify >= 0) {
       $uploaddir = $_SERVER['DOCUMENT_ROOT'] . '/Capstone-Project/Resources/Images/Furniture//'.$verify.'//';
       $fileExtension = '.jpg';
 
-      
+
       $fiName = $furnImg->countAllImages($uploaddir);
       $uploadfile = $uploaddir . basename((string)$fiName) . $fileExtension;
       echo "THE FILE NAME WILL BE ".$fiName;
@@ -76,8 +76,8 @@ if ($verify != null || $verify >= 0) {
   $uploadfile2 = $uploaddir2 . basename($_FILES['model']['name']);
 
   if ($err == 0 && move_uploaded_file($_FILES['model']['name'], $uploadfile2)) {
-    echo "NEWDATA: ".$_FILES['image']['tmp_name']."<br>"; 
-    echo "NEWDATA: ".$_FILES['model']['tmp_name']."<br>"; 
+    echo "NEWDATA: ".$_FILES['image']['tmp_name']."<br>";
+    echo "NEWDATA: ".$_FILES['model']['tmp_name']."<br>";
     echo "DIRECTORY FOR IMAGES: ".$uploaddir;
     echo "DIRECTORY FOR MODEL: ".$uploaddir2;
 
@@ -85,11 +85,11 @@ if ($verify != null || $verify >= 0) {
     echo "Upload Fail";
     echo "......uploadfile:->>>".$uploadfile;
   }
-  
+
 } else {
     echo "Connection failed";
 }
-   
 
-//header('Location: '.$_SERVER['HTTP_REFERER']);
-?> 
+
+header('Location: '.$_SERVER['HTTP_REFERER']);
+?>
