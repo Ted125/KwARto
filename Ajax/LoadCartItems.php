fb5a1dad43944bf66906fc431e0384efe3f18ee3
Christian Ted Ochoa <ochoatechmaster25@gmail.com>
2018-03-07 18:32:10 +0800

    Fixed bug where an incorrect quantity of a cart item is displayed

--- Ajax/LoadCartItems.php
+++ Ajax/LoadCartItems.php
@@ -7,7 +7,7 @@
   if($connection){
     $customerId = $_POST["customerId"];
 
-    $query = "SELECT COUNT(s.furnitureId) AS numItems, f.*, SUM(f.weight) AS totalWeight, i.image, IFNULL(w.wishlistId, '-1') AS wishlistId FROM furniture_stock AS s JOIN furniture AS f ON s.furnitureId = f.furnitureId JOIN furniture_image AS i ON f.furnitureId = i.furnitureId LEFT JOIN wishlist AS w ON f.furnitureId = w.furnitureId WHERE i.thumbnail = 1 AND s.status = 'on_hold' AND s.customerId = " . $customerId . " GROUP BY s.furnitureId";
+    $query = "SELECT COUNT(DISTINCT s.stockId) AS numItems, f.*, SUM(f.weight) AS totalWeight, i.image, IFNULL(w.wishlistId, '-1') AS wishlistId FROM furniture_stock AS s JOIN furniture AS f ON s.furnitureId = f.furnitureId JOIN furniture_image AS i ON f.furnitureId = i.furnitureId LEFT JOIN wishlist AS w ON f.furnitureId = w.furnitureId WHERE i.thumbnail = 1 AND s.status = 'on_hold' AND s.customerId = " . $customerId . " GROUP BY s.furnitureId";
     $cartResult = mysqli_query($connection, $query);
 
     $rows = array();
